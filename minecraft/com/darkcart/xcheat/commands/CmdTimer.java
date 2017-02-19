b7ccbd90cde5358e3a05dd99aea50972c3c3b96b
DarkCart <darkcartgames@gmail.com>
2017-02-19 16:12:26 -0600

    messages

--- minecraft/com/darkcart/xcheat/commands/CmdTimer.java
+++ minecraft/com/darkcart/xcheat/commands/CmdTimer.java
@@ -1,6 +1,9 @@
 package com.darkcart.xcheat.commands;
 
+import com.darkcart.xcheat.Client;
 import com.darkcart.xcheat.mods.world.Timer;
+
+import net.minecraft.util.text.TextComponentString;
 
 public class CmdTimer extends Command {
 
@@ -11,6 +14,7 @@ public class CmdTimer extends Command {
 	@Override
 	public void runCommand(String s, String[] args) {
 		Timer.speed = Integer.parseInt(args[0]);
+		Client.mc.player.addChatMessage(new TextComponentString("Timer updated."));
 	}
 
 	@Override
