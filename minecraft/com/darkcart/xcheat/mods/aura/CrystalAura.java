5a3ff951ef606a3d67eae89202dda448846e51a3
x0XP <xzeroxp@hotmail.co.uk>
2017-03-01 20:47:30 +0000

    Changes to CrystalAura - still needs testing
    Changes to CrystalAura which SHOULD autoswitch the current held item to
    a crystal, still needs to be tested so test to make sure it works.

--- minecraft/com/darkcart/xcheat/mods/aura/CrystalAura.java
+++ minecraft/com/darkcart/xcheat/mods/aura/CrystalAura.java
@@ -6,6 +6,10 @@ import com.darkcart.xcheat.Wrapper;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityEnderCrystal;
+import net.minecraft.inventory.ClickType;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemEndCrystal;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 
 public class CrystalAura extends Module {
@@ -17,19 +21,48 @@ public class CrystalAura extends Module {
 
 	@Override
 	public void disable() {
-
+		Wrapper.getMinecraft().gameSettings.keyBindUseItem.pressed = false;
 	}
 
 	@Override
 	public void tick() {
 		for (Entity e : Wrapper.getWorld().loadedEntityList) {
-			if (Wrapper.getPlayer().getDistanceToEntity(e) > 5 && e != Wrapper.getPlayer()) {
-				Wrapper.getMinecraft().gameSettings.keyBindUseItem.pressed = true;
-				if (e instanceof EntityEnderCrystal) {
+			if (Wrapper.getPlayer().getDistanceToEntity(e) > 5 && e != Wrapper.getPlayer() && Wrapper.getPlayer().canEntityBeSeen(e)) {
+				placeCrystal();
+				if (e instanceof EntityEnderCrystal) { //attack crystal
 					Client.mc.playerController.attackEntity(Client.mc.player, e);
+					Wrapper.getPlayer().swingArm(EnumHand.MAIN_HAND);
 				}
-				Wrapper.getPlayer().swingArm(EnumHand.MAIN_HAND);
 				Wrapper.getMinecraft().gameSettings.keyBindUseItem.pressed = false;
+			}
+		}
+	}
+	
+	private void placeCrystal() {
+		for(int slot = 44; slot >= 9; slot--) {
+			ItemStack stack = Wrapper.getPlayer().inventoryContainer.getSlot(slot).getStack();
+			
+			
+			if(stack != null) {
+				if(slot >= 36 && slot <= 44) {
+					if(stack.getItem() instanceof ItemBlock 
+							&& (stack.getItem() instanceof ItemEndCrystal)) {
+						Wrapper.getPlayer().inventory.currentItem = slot - 36;
+						Wrapper.getMinecraft().gameSettings.keyBindUseItem.pressed = true;
+					}
+				} else if(stack.getItem() instanceof ItemBlock 
+						&& (stack.getItem() instanceof ItemEndCrystal)) {
+					int itemSlot = slot;
+					int currentSlot = Wrapper.getPlayer().inventory.currentItem + 36;
+					Wrapper.getMinecraft().playerController.windowClick(0, slot, 0, ClickType.PICKUP_ALL, Wrapper.getPlayer());
+					Wrapper.getMinecraft().playerController.windowClick(0, currentSlot, 0, ClickType.SWAP, Wrapper.getPlayer());
+					for(slot = 44; slot >= 9; slot--) {
+						if(Wrapper.getPlayer().inventoryContainer.getSlot(slot).getStack() == null)
+						{
+							Wrapper.getMinecraft().playerController.windowClick(0, slot, 0, ClickType.SWAP, Wrapper.getPlayer());
+						}
+					}
+				}
 			}
 		}
 	}
