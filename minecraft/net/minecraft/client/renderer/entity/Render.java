d2732ea804bdc246099f3d59d70fe8b29ff44751
x0XP <xzeroxp@hotmail.co.uk>
2017-03-05 21:43:39 +0000

    HealthTags changes
    fixing yet another crash

--- minecraft/net/minecraft/client/renderer/entity/Render.java
+++ minecraft/net/minecraft/client/renderer/entity/Render.java
@@ -377,8 +377,8 @@ public abstract class Render<T extends Entity>
             boolean flag1 = this.renderManager.options.thirdPersonView == 2;
             float f2 = entityIn.height + 0.5F - (flag ? 0.25F : 0.0F);
             int i = "deadmau5".equals(str) ? -10 : 0;
-            EntityLiving entity = ((EntityLiving) entityIn);
             if (Client.findMod(HealthTags.class).isToggled()) {
+                EntityPlayer entity = ((EntityPlayer) entityIn);
             	str = str + " \247a" + ((double)Math.round((entity.getHealth() * 100) / 100) / 2);
             }
             EntityRenderer.drawNameplate(this.getFontRendererFromRenderManager(), str, (float)x, (float)y + f2, (float)z, i, f, f1, flag1, flag);
