edee0eae9cb830d28783bffa39155ff971986846
x0XP <xzeroxp@hotmail.co.uk>
2017-03-14 14:35:35 +0000

    Tracers changes
    Tracers now have a different color for players that are friends and only
    draws to players rather than all entities.

--- minecraft/com/darkcart/xdolf/mods/render/Tracers.java
+++ minecraft/com/darkcart/xdolf/mods/render/Tracers.java
@@ -36,29 +36,35 @@ public class Tracers extends Module {
 				GL11.glLineWidth(1.5F);
 				for (Entity entities : Client.mc.world.loadedEntityList) {
 					if (entities != Client.mc.player && entities != null) {
-						float distance = Client.mc.renderViewEntity.getDistanceToEntity(entities);
-						double posX = ((entities.lastTickPosX + (entities.posX - entities.lastTickPosX)
-								- RenderManager.renderPosX));
-						double posY = ((entities.lastTickPosY + (entities.posY - entities.lastTickPosY)
-								- RenderManager.renderPosY));
-						double posZ = ((entities.lastTickPosZ + (entities.posZ - entities.lastTickPosZ)
-								- RenderManager.renderPosZ));
-	
-						if (distance <= 6F) {
-							GL11.glColor3f(1.0F, 0.0F, 0.0F);
-						} else if (distance <= 96F) {
-							GL11.glColor3f(1.0F, (distance / 100F), 0.0F);
-						} else if (distance > 96F) {
-							GL11.glColor3f(0.1F, 0.6F, 255.0F);
+						if (entities instanceof EntityPlayer) {
+							float distance = Client.mc.renderViewEntity.getDistanceToEntity(entities);
+							double posX = ((entities.lastTickPosX + (entities.posX - entities.lastTickPosX)
+									- RenderManager.renderPosX));
+							double posY = ((entities.lastTickPosY + (entities.posY - entities.lastTickPosY)
+									- RenderManager.renderPosY));
+							double posZ = ((entities.lastTickPosZ + (entities.posZ - entities.lastTickPosZ)
+									- RenderManager.renderPosZ));
+		
+							if(Wrapper.getFriends().isFriend(entities.getName())) {
+		    					GL11.glColor3f(0.0F, 1.0F, 0.0F);
+		    				}else{
+								if (distance <= 6F) {
+									GL11.glColor3f(1.0F, 0.0F, 0.0F);
+								} else if (distance <= 96F) {
+									GL11.glColor3f(1.0F, (distance / 100F), 0.0F);
+								} else if (distance > 96F) {
+									GL11.glColor3f(0.1F, 0.6F, 255.0F);
+								}
+		    				}
+							Vec3d eyes = new Vec3d(0, 0, 1).rotatePitch(-(float) Math.toRadians(Wrapper.getPlayer().rotationPitch)).rotateYaw(-(float) Math.toRadians(Wrapper.getPlayer().rotationYaw));
+							
+							GL11.glBegin(GL11.GL_LINE_LOOP);
+							
+							GL11.glVertex3d(eyes.xCoord, Wrapper.getPlayer().getEyeHeight() + eyes.yCoord, eyes.zCoord);
+							GL11.glVertex3d(posX, posY, posZ);
+		
+							GL11.glEnd();
 						}
-						Vec3d eyes = new Vec3d(0, 0, 1).rotatePitch(-(float) Math.toRadians(Wrapper.getPlayer().rotationPitch)).rotateYaw(-(float) Math.toRadians(Wrapper.getPlayer().rotationYaw));
-						
-						GL11.glBegin(GL11.GL_LINE_LOOP);
-						
-						GL11.glVertex3d(eyes.xCoord, Wrapper.getPlayer().getEyeHeight() + eyes.yCoord, eyes.zCoord);
-						GL11.glVertex3d(posX, posY, posZ);
-	
-						GL11.glEnd();
 					}
 				}
 				GL11.glDisable(GL11.GL_BLEND);
