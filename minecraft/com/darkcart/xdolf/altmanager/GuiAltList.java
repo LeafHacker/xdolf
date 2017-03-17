f5fbfd088e84e34d0024b7364c155f4c917f8a5c
x0XP <xzeroxp@hotmail.co.uk>
2017-03-17 19:39:05 +0000

    Fixed issues in Alt Manager
    Fixed issue where you couldn't click slots in the Alt Manager. Still
    needs fixes for loading alts.

--- minecraft/com/darkcart/xdolf/altmanager/GuiAltList.java
+++ minecraft/com/darkcart/xdolf/altmanager/GuiAltList.java
@@ -1,128 +1,147 @@
 package com.darkcart.xdolf.altmanager;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import com.darkcart.xdolf.Client;
-import com.darkcart.xdolf.Wrapper;
-
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.gui.GuiMultiplayer;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiYesNo;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.util.Session;
+
+import java.io.IOException;
+
+import com.darkcart.xdolf.Wrapper;
 
 public class GuiAltList extends GuiScreen {
 	public String dispErrorString = "";
 	public boolean deleteMenuOpen = false;
-	
+
 	public GuiAltList() {
 		Wrapper.getFileManager().loadAlts();
 	}
-	
+
 	public FontRenderer getLocalFontRenderer() {
 		return this.fontRendererObj;
 	}
-	
+
 	public void onGuiClosed() {
 		Wrapper.getFileManager().saveAlts();
 		super.onGuiClosed();
 	}
-	
+
 	private SlotAlt tSlot;
-	
+
 	public void initGui() {
 		buttonList.clear();
-		buttonList.add(new GuiButton(1, width / 2 - 100, height - 47, 66, 20, "Add"));
-		buttonList.add(new GuiButton(2, width / 2 - 33, height - 47, 65, 20, "Login"));
-		buttonList.add(new GuiButton(3, width / 2 - 100, height - 26, 66, 20, "Remove"));
-		buttonList.add(new GuiButton(4, width / 2 - 33, height - 26, 64, 20, "Back"));
-		buttonList.add(new GuiButton(5, width / 2 + 32, height - 26, 69, 20, "Direct Login"));
-		
-		tSlot = new SlotAlt(Client.mc, this);
+		buttonList.add(new GuiButton(5, width / 2 - 105, height - 47, 100, 20,
+				"Direct Login"));
+		buttonList.add(new GuiButton(2, width / 2 + 5, height - 47, 100, 20,
+				"Login"));
+
+		buttonList.add(new GuiButton(1, width / 2 - 105, height - 26, 66, 20,
+				"Add"));
+		buttonList.add(new GuiButton(3, width / 2 - 33, height - 26, 66, 20,
+				"Remove"));
+		buttonList.add(new GuiButton(4, width / 2 + 39, height - 26, 66, 20,
+				"Cancel"));
+
+		tSlot = new SlotAlt(Minecraft.getMinecraft(), this);
 		tSlot.registerScrollButtons(7, 8);
 	}
-	
+
+	/**
+	 * Handles mouse input.
+	 */
+	public void handleMouseInput() throws IOException {
+		super.handleMouseInput();
+		this.tSlot.handleMouseInput();
+	}
+
 	@Override
 	public void confirmClicked(boolean flag, int i1) {
 		super.confirmClicked(flag, i1);
-		if(deleteMenuOpen) {
+		if (deleteMenuOpen) {
 			deleteMenuOpen = false;
-			
-			if(flag) {
+
+			if (flag) {
 				Manager.altList.remove(i1);
 				Wrapper.getFileManager().saveAlts();
 			}
-			
+
 			this.mc.displayGuiScreen(this);
 		}
 	}
-	
-	public void actionPerformed(GuiButton button) throws IOException {
-		super.actionPerformed(button);
-		if(button.id == 1) {
+
+	public void actionPerformed(GuiButton button) {
+		try {
+			super.actionPerformed(button);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (button.id == 1) {
 			GuiAltAdd gaa = new GuiAltAdd(this);
 			this.mc.displayGuiScreen(gaa);
 		}
-		if(button.id == 2) {
+		if (button.id == 2) {
 			try {
 				Alt a1 = Manager.altList.get(tSlot.getSelected());
-				if(a1.isPremium()) {
+				if (a1.isPremium()) {
 					try {
-			        	YggdrasilAuthenticator auth = new YggdrasilAuthenticator(a1.getUsername(), a1.getPassword());
-			            if (auth.login()) {
-			                Client.mc.session = auth.getSession();
-			            }
+						Session s = YggdrasilPayload.loginPassword(
+								a1.getUsername(), a1.getPassword());
+						if (s != null) {
+							Minecraft.getMinecraft().session = s;
+						}
 						Manager.altScreen.dispErrorString = "";
-					} catch(Exception error) {
-						dispErrorString = "".concat("\247cBad Login \2477(").concat(a1.getUsername()).concat(")");
+					} catch (Exception error) {
+						dispErrorString = "".concat("\247cBad Login \2477(")
+								.concat(a1.getUsername()).concat(")");
 					}
 				} else {
+					Minecraft.getMinecraft().session = YggdrasilPayload
+							.loginCrack(a1.getUsername());
 					Manager.altScreen.dispErrorString = "";
 				}
-			}catch(Exception e) {}
+			} catch (Exception e) {
+			}
 		}
-		if(button.id == 3)
-		{
-			try
-			{
-                String s1 = I18n.format("Are you sure you want to delete the alt: " + "\"" + Manager.altList.get(tSlot.getSelected()).getUsername() + "\"" + "?", new Object[0]);
-                String s3 = I18n.format("Delete", new Object[0]);
-                String s4 = I18n.format("Cancel", new Object[0]);
-				GuiYesNo guiyesno = new GuiYesNo(this, s1, "", s3, s4, tSlot.getSelected());
+		if (button.id == 3) {
+			try {
+				String s1 = "Delete the alt: "
+						+ "\""
+						+ Manager.altList.get(tSlot.getSelected())
+								.getUsername() + "\"" + "?";
+				String s3 = "Delete";
+				String s4 = "Cancel";
+				GuiYesNo guiyesno = new GuiYesNo(this, s1, "", s3, s4,
+						tSlot.getSelected());
 				deleteMenuOpen = true;
 				this.mc.displayGuiScreen(guiyesno);
-			}catch(Exception e) {}
+			} catch (Exception e) {
+			}
 		}
-		if(button.id == 4)
-		{
-			this.mc.displayGuiScreen(new GuiMultiplayer(new GuiMainMenu()));
+		if (button.id == 4) {
+			this.mc.displayGuiScreen(new GuiMainMenu());
 		}
-		if(button.id == 5)
-		{
+		if (button.id == 5) {
 			GuiDirectLogin gdl = new GuiDirectLogin(this);
 			this.mc.displayGuiScreen(new GuiDirectLogin(this));
 		}
 	}
-	
-	public void updateScreen()
-	{
+
+	public void updateScreen() {
 		super.updateScreen();
 	}
-	
-	public void drawScreen(int mouseX, int mouseY, float partialTicks)
-	{
-		tSlot.drawScreen(mouseX, mouseY, partialTicks);
-		drawCenteredString(this.fontRendererObj, "Alts: \2477" + Manager.altList.size(), width / 2, 13, 0xFFFFFF);
-		this.fontRendererObj.drawStringWithShadow("Username: \2477" + Client.mc.session.username, 3, 3, 0xFFFFFF);
-		this.fontRendererObj.drawStringWithShadow(dispErrorString, 3, 13, 0xFFFFFF); //drawStringWithShadow
-		super.drawScreen(mouseX, mouseY, partialTicks);
+
+	public void drawScreen(int i, int j, float f) {
+		tSlot.drawScreen(i, j, f);
+		drawCenteredString(this.fontRendererObj, "Alts", width / 2, 13,
+				0xFFFFFF);
+		this.fontRendererObj.drawStringWithShadow(
+				"Playing as " + Minecraft.getMinecraft().session.getUsername(),
+				3, 3, 0xFFFFFF);
+		this.fontRendererObj.drawStringWithShadow(dispErrorString, 3, 13,
+				0xFFFFFF);
+		super.drawScreen(i, j, f);
 	}
 }
--- minecraft/com/darkcart/xdolf/altmanager/SlotAlt.java
+++ minecraft/com/darkcart/xdolf/altmanager/SlotAlt.java
@@ -1,14 +1,10 @@
 package com.darkcart.xdolf.altmanager;
 
-import java.util.ArrayList;
-
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiSlot;
-import net.minecraft.client.renderer.ImageBufferDownload;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.util.StringUtils;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class SlotAlt extends GuiSlot
 {
@@ -19,13 +15,7 @@ public class SlotAlt extends GuiSlot
 	{
 		super(theMinecraft, aList.width, aList.height, 32, aList.height - 59, Manager.slotHeight);
 		this.aList = aList;
-		this.selected = 0;
-	}
-	
-	@Override
-	protected int getContentHeight()
-	{
-		return this.getSize() * Manager.slotHeight;
+		this.selected = -1;
 	}
 	
 	@Override
@@ -58,23 +48,37 @@ public class SlotAlt extends GuiSlot
 	}
 
 	@Override
-	protected void drawSlot(int selectedIndex, int x, int y, int var4, int var6, int var7)
-	{
-		//try
-		//{
-			Alt theAlt = Manager.altList.get(selectedIndex);
-			aList.drawString(aList.getLocalFontRenderer(), theAlt.getUsername(), x, y + 1, 0xFFFFFF);
-			if(theAlt.isPremium())
-			{
-				aList.drawString(aList.getLocalFontRenderer(), "Premium", x, y + 12, 0x00FF00);
-				//aList.drawString(aList.getLocalFontRenderer(), Manager.makePassChar(theAlt.getPassword()), x, y + 12, 0x808080);
-			}else
-			{
-				aList.drawString(aList.getLocalFontRenderer(), "\247mPremium", x, y + 12, 0x990000);
-			}
-		/*}catch(AccountManagementException error)
+	protected void drawSlot(int var1, int var2, int var3, int var4, int var5, int var6) {
+		Alt theAlt = Manager.altList.get(var1);
+		aList.drawString(aList.getLocalFontRenderer(), theAlt.getUsername(), var2, var3 + 1, 0xFFFFFF);
+		if(theAlt.isPremium())
 		{
-			error.printStackTrace();
-		}*/
+			aList.drawString(aList.getLocalFontRenderer(), "Premium", var2, var3 + 12, 0x00FF00);
+		}else
+		{
+			aList.drawString(aList.getLocalFontRenderer(), "\247mPremium", var2, var3 + 12, 0x990000);
+		}
+
 	}
+
+    public void drawTexturedModalRectZ(final float i, final float y, final int k, final int i1, final int j1, final int k1, final float zLevel) {
+		float f = 0.015625F;
+		float f1 = 0.03125F;
+		Tessellator tesselator = Tessellator.getInstance();
+		VertexBuffer vertexbuffer = tesselator.getBuffer();
+		vertexbuffer.begin(7, DefaultVertexFormats.POSITION_TEX);
+/*
+		tesselator.getWorldRenderer().addVertexWithUV(i + 0, y + k1, zLevel, (k + 0) * f, (i1 + k1) * f1);
+		tesselator.getWorldRenderer().addVertexWithUV(i + j1, y + k1, zLevel, (k + j1) * f, (i1 + k1) * f1);
+		tesselator.getWorldRenderer().addVertexWithUV(i + j1, y + 0, zLevel, (k + j1) * f, (i1 + 0) * f1);
+		tesselator.getWorldRenderer().addVertexWithUV(i + 0, y + 0, zLevel, (k + 0) * f, (i1 + 0) * f1);
+		*/
+		
+		vertexbuffer.pos(i + 0, y + k1, zLevel);
+		vertexbuffer.pos(i + j1, y + k1, zLevel);
+		vertexbuffer.pos(i + j1, y + 0, zLevel);
+		vertexbuffer.pos(i + 0, y + 0, zLevel);
+
+		tesselator.draw();
+    }
 }
