a5b8e654f0c71c442468a74a35a895506cc6da52
x0XP <xzeroxp@hotmail.co.uk>
2017-04-16 20:09:56 +0100

    Probably, hopefully fix carriage return #2

--- minecraft/com/darkcart/xdolf/util/Splashes.java
+++ minecraft/com/darkcart/xdolf/util/Splashes.java
@@ -14,6 +14,8 @@ import javax.net.ssl.X509TrustManager;
 
 import com.darkcart.xdolf.Client;
 
+import net.minecraft.util.Util;
+
 public class Splashes {
 
 	public static String[] getSplashes() {
@@ -51,6 +53,11 @@ public class Splashes {
 			}
 
 			bufferedReader.close();
+			
+			Util.EnumOS util$enumos = Util.getOSType();
+			if (util$enumos != Util.EnumOS.WINDOWS)
+				return stringBuilder.toString().substring(0, stringBuilder.toString().length()).split("\n");
+			
 			return stringBuilder.toString().substring(0, stringBuilder.toString().length()).split("\r\n");
 		} catch (Exception e) {
 		}
